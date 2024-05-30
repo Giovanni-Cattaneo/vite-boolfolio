d1811e2a5835aa46efa6da325bdd887ce5fc454f
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-30 21:37:31 +0200

    edit color palette

--- src/App.vue
+++ src/App.vue
@@ -13,48 +13,28 @@ export default {
   name: 'App',
   data() {
     return {
-      projects: [],
-      base_api: 'http://127.0.0.1:8000',
-      url_project: '/api/projects'
 
     }
   },
   methods: {
-    callApi() {
-      axios.get(this.base_api + this.url_project).then(response => {
-        console.log(response);
-        this.projects = response.data.projects
-      }).catch(error => {
-        console.error('Error fetching projects:', error);
-      });
-    },
-    cardDetail() {
-      console.log('Hello')
-    }
   },
-  mounted() {
-    this.callApi();
-  }
+
 }
 </script>
 
 <template>
   <appHeader />
 
-  <div class="container d-flex mt-5">
-    <div class="row">
-      <div class="col-12 col-sm-6 col-md-4 col-lg-3 g-5" v-for="project in projects.data" :key="project.id">
-        <div class="card" @click="cardDetail">
-          <img class="card-img-top" :src="base_api + '/storage/' + project.cover_image" alt="Title" />
-          <div class="card-body">
-            <h4 class="card-title">{{ project.title }}</h4>
-            <p class="card-text">{{ project.description }}</p>
-          </div>
-        </div>
+  <main class="bg-light">
+    <div class="container d-flex mt-5 ">
+      <div class="row">
+        <appCard />
       </div>
     </div>
-  </div>
+  </main>
 
+
+  <appFooter />
 </template>
 
 <style></style>
--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -13,9 +13,33 @@ export default {
 </script>
 
 <template>
-    <header>
+    <footer class="bg-dark text-light  mt-5">
+        <div class="container d-flex justify-content-between py-3">
+            <ul class="list-unstyled">
+                <li><a href="" class="text-light text-decoration-none">Lorem, ipsum.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Modi, quae.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Odio, aperiam.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Dolorum, deleniti.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Adipisci, quibusdam?</a></li>
+            </ul>
+            <ul class="list-unstyled">
+                <li><a href="" class="text-light text-decoration-none">Lorem, ipsum.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Reiciendis, facere?</a></li>
+                <li><a href="" class="text-light text-decoration-none">Voluptate, provident?</a></li>
+                <li><a href="" class="text-light text-decoration-none">Quo, repudiandae.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Laudantium, voluptates.</a></li>
+            </ul>
+            <ul class="list-unstyled">
+                <li><a href="" class="text-light text-decoration-none">Lorem, ipsum.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Cumque, aut.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Voluptatibus, libero!</a></li>
+                <li><a href="" class="text-light text-decoration-none">Eaque, pariatur.</a></li>
+                <li><a href="" class="text-light text-decoration-none">Iusto, natus?</a></li>
+            </ul>
+        </div>
 
-    </header>
+
+    </footer>
 </template>
 
-<style></style>
+<style scoped></style>
