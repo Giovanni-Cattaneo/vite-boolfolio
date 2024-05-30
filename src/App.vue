d1b67ad3a60ae8da5f07bf47c7df3703f1cd57d1
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-30 16:08:59 +0200

    add appcard component, working on img

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,25 @@
 import axios from 'axios';
 import appHeader from "./components/appHeader.vue";
 import appFooter from "./components/appFooter.vue";
+import appCard from "./components/appCard.vue"
 
 export default {
   components: {
     appHeader,
     appFooter,
+    appCard,
   },
   name: 'App',
   data() {
     return {
       projects: [],
+      base_api: 'http://127.0.0.1:8000/api/projects/',
     }
   },
   methods: {
     callApi() {
-      axios.get('http://127.0.0.1:8000/api/projects').then(response => {
-        //console.log(response);
+      axios.get(this.base_api).then(response => {
+        console.log(response);
         this.projects = response.data.projects
       }).catch(error => {
         console.error('Error fetching projects:', error);
@@ -33,11 +36,18 @@ export default {
 <template>
   <appHeader />
 
-  <div class="d-flex" v-for="project in projects.data" :key="project.id">
-    <h1>
-      {{ project.title }}
-    </h1>
-    <p>{{ project.description }}</p>
+  <div class="container d-flex mt-5">
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3 g-5" v-for="project in projects.data" :key="project.id">
+        <div class="card">
+          <img class="card-img-top" :src="base_api + 'storage/' + project.cover_image" alt="Title" />
+          <div class="card-body">
+            <h4 class="card-title">{{ project.title }}</h4>
+            <p class="card-text">{{ project.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ src/components/appCard.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+    name: 'appcard',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+    },
+}
+</script>
+
+<template>
+    <header>
+
+    </header>
+</template>
+
+<style></style>
