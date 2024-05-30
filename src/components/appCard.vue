0e6cc31c917be0f14b2a2f58b852fd57be623aff
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-30 16:44:13 +0200

    add appCard

--- src/components/appCard.vue
+++ src/components/appCard.vue
@@ -1,21 +1,45 @@
 <script>
+import axios from 'axios';
 
 export default {
-    name: 'appcard',
+    name: 'appCard',
     data() {
         return {
-
+            projects: [],
+            base_api: 'http://127.0.0.1:8000',
+            url_project: '/api/projects'
         }
     },
     methods: {
+        callApi() {
+            axios.get(this.base_api + this.url_project).then(response => {
+                console.log(response);
+                this.projects = response.data.projects
+            }).catch(error => {
+                console.error('Error fetching projects:', error);
+            });
+        }
     },
+    mounted() {
+        this.callApi();
+    }
 }
 </script>
 
 <template>
-    <header>
-
-    </header>
+    <div class="container d-flex mt-5">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 g-5" v-for="project in projects.data" :key="project.id">
+                <div class="card">
+                    <img class="card-img-top" :src="base_api + '/storage/' + project.cover_image" alt="Title" />
+                    <div class="card-body">
+                        <h4 class="card-title">{{ project.title }}</h4>
+                        <p class="card-text">{{ project.description }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style></style>
