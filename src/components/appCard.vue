6f66e9879fc601b0b6e488596272c31a6f6ca7c6
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-31 09:31:40 +0200

    add types with conditional

--- src/components/appCard.vue
+++ src/components/appCard.vue
@@ -14,7 +14,8 @@ export default {
         callApi() {
             axios.get(this.base_api + this.url_project).then(response => {
                 // console.log(response);
-                this.projects = response.data.projects
+                this.projects = response.data.projects.data
+                console.log(response)
             }).catch(error => {
                 console.error('Error fetching projects:', error);
             });
@@ -31,12 +32,13 @@ export default {
 
 <template>
 
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3 g-5" v-for="project in projects.data" :key="project.id">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 g-5" v-for="project in projects" :key="project.id">
         <div class="card" @click="cardDetail">
             <img class="card-img-top" :src="base_api + '/storage/' + project.cover_image" alt="Title" />
             <div class="card-body">
                 <h4 class="card-title">{{ project.title }}</h4>
                 <p class="card-text">{{ project.description }}</p>
+                <p v-if="project.type">{{ project.type.category }}</p>
             </div>
         </div>
     </div>
