616be3dfa6f52d6eee3c6792eb9daf221b2313d3
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-06-02 16:06:41 +0200

    add generation for favourites

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,5 +1,6 @@
 <script>
 import ProjectsList from "./ProjectsList.vue";
+import axios from 'axios';
 
 export default {
     components: {
@@ -7,7 +8,27 @@ export default {
     },
     data() {
         return {
+            projects: [],
+            base_api: 'http://127.0.0.1:8000',
+            url_project: '/api/projects'
         };
+    },
+    methods: {
+        callApi() {
+            axios.get(this.base_api + this.url_project).then(response => {
+                console.log(response);
+                this.projects = response.data.projects.data
+                console.log(response)
+            }).catch(error => {
+                console.error('Error fetching projects:', error);
+            });
+        },
+        cardDetail() {
+            console.log('Hello', this)
+        }
+    },
+    mounted() {
+        this.callApi();
     }
 };
 </script>
@@ -21,8 +42,9 @@ export default {
                         <h3>Back-end</h3>
                         <p>Abile nello sviluppo di funzionalità e programmi</p>
                     </div>
-                    <div class="m-auto text-center d-flex align-items-center justify-content-center">
+                    <div class="mt-5 text-center mx-auto">
                         <h2>Giovanni Cattaneo</h2>
+                        <p>Full Stack Web Developer</p>
                     </div>
                     <div class="front text-center">
                         <h3>Front-end</h3>
@@ -42,22 +64,8 @@ export default {
             </div>
             <div class="latest_projects pt-5">
                 <div class="row">
-                    <div class="col d-flex gap-2 justify-content-evenly flex-wrap">
-                        <div class="card">
-                            <img class="card-img-top" src="" alt="Title" />
-                            <div class="card-body">
-                                <h4 class="card-title">Title</h4>
-                                <p class="card-text">Text</p>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <img class="card-img-top" src="" alt="Title" />
-                            <div class="card-body">
-                                <h4 class="card-title">Title</h4>
-                                <p class="card-text">Text</p>
-                            </div>
-                        </div>
-                        <div class="card">
+                    <div v-for="project in projects" class="col d-flex gap-2 justify-content-evenly flex-wrap">
+                        <div class="card" v-if="project.favourites">
                             <img class="card-img-top" src="" alt="Title" />
                             <div class="card-body">
                                 <h4 class="card-title">Title</h4>
