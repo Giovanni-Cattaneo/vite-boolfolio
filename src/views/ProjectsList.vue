bfc637c50929cf23256fca1b143fb6d91ca287dd
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-06-03 16:53:09 +0200

    edit prev and next methods in project list

--- src/views/ProjectsList.vue
+++ src/views/ProjectsList.vue
@@ -20,7 +20,8 @@ export default {
 </script>
 
 <template>
-    <div class="container">
+    <div class="container text-dark">
+        <h2 class="mt-3">Complete Project List</h2>
         <div class="row">
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 g-5" v-for="project in state.projects.data" :key="project.id">
                 <div class="card" @click="cardDetail">
@@ -43,7 +44,7 @@ export default {
             <ul class="pagination">
                 <!-- Previous -->
                 <li class="page-item" :class="{ disabled: !state.projects.prev_page_url }">
-                    <button class="page-link" aria-label="Previous" @click="prev(state.projects.prev_page_url)"
+                    <button class="page-link" aria-label="Previous" @click="state.prev(state.projects.prev_page_url)"
                         :disabled="!state.projects.prev_page_url">
                         <span aria-hidden="true">&laquo;</span>
                     </button>
@@ -52,12 +53,12 @@ export default {
                 <!-- Pages -->
                 <li class="page-item" v-for="page in state.projects.last_page" :key="page"
                     :class="{ active: page === state.projects.current_page }">
-                    <button class="page-link" @click="choosePage(page)">{{ page }}</button>
+                    <button class="page-link" @click="state.choosePage(page)">{{ page }}</button>
                 </li>
 
                 <!-- Next -->
                 <li class="page-item" :class="{ disabled: !state.projects.next_page_url }">
-                    <button class="page-link" aria-label="Next" @click="next(state.projects.next_page_url)"
+                    <button class="page-link" aria-label="Next" @click="state.next(state.projects.next_page_url)"
                         :disabled="!state.projects.next_page_url">
                         <span aria-hidden="true">&raquo;</span>
                     </button>
