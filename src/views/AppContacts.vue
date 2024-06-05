04d55d18a5b61b1e0dcaf8926f5eb0053e507cf4
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-06-05 12:31:23 +0200

    add simple form

--- src/views/AppContacts.vue
+++ src/views/AppContacts.vue
@@ -9,9 +9,18 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1 class="text-light">Welcome to App Home</h1>
-        <p>This is the home page </p>
+    <div class="container">
+        <h1>Welcome to Contacts page, keep in touch</h1>
+
+        <form action="" method="POST">
+            <label for="name">Nome:</label>
+            <input type="text" id="name" name="name"><br><br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email"><br><br>
+            <label for="message">Messaggio:</label>
+            <textarea id="message" name="message"></textarea><br><br>
+            <button type="submit">Invia</button>
+        </form>
     </div>
 </template>
 
